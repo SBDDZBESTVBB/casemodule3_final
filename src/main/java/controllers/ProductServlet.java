2940865bcccc8871029dc742b4451b26ead940f4
SBDDZBESTVBB <114671739+SBDDZBESTVBB@users.noreply.github.com>
2023-01-03 15:43:46 +0700

    update search

---
+++ src/main/java/controllers/ProductServlet.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import models.Product;
+
+import services.ProductService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(urlPatterns = "/ProductServlet")
+public class ProductServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String search=request.getParameter("search");
+        List<Product> product = ProductService.getSearchProduct(search);
+        request.setAttribute("products", product);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/view-product.jsp");
+        requestDispatcher.forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
